b290090a32af27e5a5cb37e8b5942dd6b577e7bd
Jeremy Bchiri <jerem.b95@gmail.com>
2013-12-11 20:49:23 +0100

    Modif bdd
    fichier de connexion sql.php

--- model/sql.php
+++ model/sql.php
@@ -1,8 +1,8 @@
 <?php
 
-$dns = 'mysql:host=mysql51-104.perso;dbname=shiftie_db';
-$utilisateur = 'shiftie_db';
-$mdp = 'shiftie33';
+$dns = 'mysql:host=localhost;dbname=shiftie_db';
+$utilisateur = 'root';
+$mdp = 'root';
 
 $options = array(
 	PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8",
