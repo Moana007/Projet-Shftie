4c659aad09d2f5ff1bdfe5dae0b0c9315e32ce96
Guillaume LEGAIE <moana974@wanadoo.fr>
2013-12-20 14:43:31 +0100

    maj

---
+++ view/home/catalogue/new.php
@@ -0,0 +1,18 @@
+<?php foreach ($new as $new): ?>
+      <div class="bloc_recipe1 bloc_validate">
+          <a href="?appli=home&action=recipe&id_rec="<?php $recipe['recettes_id']; ?>"><img src="<?php echo $new['photo']; ?>">
+            <div class="info1">
+              <a href="#"><?php echo $new['nom']; ?></a>
+            </div>
+          </a>
+        </div
+
+        >
+      <?php endforeach; ?>
+        <div class="pagination">
+          <a class="bloc_pagination" href="#"><span class="icon icon-left"></span></a>
+          <a class="bloc_chiffre actif" href="#">1</a>
+          <a class="bloc_chiffre" href="#">2</a>
+          <a class="bloc_chiffre" href="#">3</a>
+          <a class="bloc_pagination" href="#"><span class="icon icon-right"></span></a>
+        </div>
