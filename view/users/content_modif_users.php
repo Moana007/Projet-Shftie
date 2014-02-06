6226de24265fbfc87871626cdaf10b347c904381
Moana007 <moana974@wanadoo.fr>
2014-02-06 14:40:40 +0100

    Debug modify users

--- view/users/content_modif_users.php
+++ view/users/content_modif_users.php
@@ -39,15 +39,15 @@
       <form class="" method="post" id="users_udpate" action="?appli=users&action=modif_users">
         <?php foreach($authors as $authors): ?>
 
-          <p>Age<input type="text" name="age" value='<?php echo $authors['age']; ?>' ></p>
+          <p>Age<input type="text" name="age" value='<?php echo $authors["age"]; ?>' ></p>
           <br/>
-          <p>City<input type="text" name="city" value='<?php echo $authors['city']; ?>' ></p>
+          <p>City<input type="text" name="city" value='<?php echo $authors["city"]; ?>' ></p>
           <br/>
-          <p>Favorite plate<input type="text" name="favorite_plate" value='<?php echo $authors['favorite_plate']; ?>' ></p>
+          <p>Favorite plate<input type="text" name="favorite_plate" value='<?php echo $authors["favorite_plate"]; ?>' ></p>
           <br/>
-          <p>Description of you<input type="text" name="users_desc" value='<?php echo $authors['users_description']; ?>' ></p>
+          <p>Description of you<input type="text" name="users_desc" value='<?php echo $authors["users_description"]; ?>' ></p>
           <br/>
-          <p>Your preference <input type="text" name="pref" value='<?php echo $authors['users_favorite']; ?>' ></p>
+          <p>Your preference <input type="text" name="pref" value='<?php echo $authors["users_favorite"]; ?>' ></p>
           <br/>
 
           <!-- Bouton radio pour choix du sexe, affichage en fonction de son état -->
